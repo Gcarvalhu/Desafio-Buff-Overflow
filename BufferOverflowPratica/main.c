8971abc38968fada14de6c94744128ad4e318ec7
Gcarvalhu <gustavo190371@gmail.com>
2021-08-09 12:52:02 -0300

    desafio

---
+++ BufferOverflowPratica/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+	
+int main()
+{
+  char str0[6]="98765",str1[5]="abcd",str2[10]="efghijklm";
+
+//   printf("str0 = %s\t str1 = %s\t str2 = %s\n",str0,str1,str2);
+//   strcpy(str1,"1234567");
+   printf("str0 = %d \n str2 = %s \n str1 = %s",str0,str1,str2);
+}
